e044a5e31b9f1ca6f96da9c4e56d55b2326efd36
Max <shidoutsuruya@gmail.com>
2023-11-02 11:34:01 +0800

    authentication

--- Social_Website/settings.py
+++ Social_Website/settings.py
@@ -146,8 +146,9 @@ SOCIAL_AUTH_FACEBOOK_KEY=FACEBOOK_KEY
 SOCIAL_AUTH_FACEBOOK_SECRET=FACEBOOK_VALUE
 SOCIAL_AUTH_FACEBOOK_SCOPE=['email']
 SOCIAL_AUTH_TWITTER_KEY=TWITTER_KEY
-SOCIAL_AUTH_TWITTER_VALUE=TWITTER_VALUE
-
+SOCIAL_AUTH_TWITTER_SECRET=TWITTER_VALUE
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY=GOOGLE_KEY
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=GOOGLE_VALUE
 #SOCIAL PIPELINE
 SOCIAL_AUTH_PIPELINE=[
     'social_core.pipeline.social_auth.social_details',
@@ -156,7 +157,7 @@ SOCIAL_AUTH_PIPELINE=[
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'account.pipeline.create_profile',
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
